1289fa36fbb79710050c16d0724dd0b50acec49d
Smirnov Ivan <ivan.smirnov.95@mail.ru>
2017-04-04 17:43:45 +0300

    Initial Commit

---
+++ raspad/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  raspad
+//
+//  Created by Smirnov Ivan on 04.04.17.
+//  Copyright © 2017 bmstu sm3. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
